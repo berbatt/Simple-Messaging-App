8469ab52832c6dbd8bb45b6e01b9fb9793fcb177
berbatt <berkay.bat@gmail.com>
2022-01-01 15:49:02 +0300

    Server class with basic methods are implemented

---
+++ Server.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+import threading
+
+
+def handleClientConnection(clientSocket, clientAddress):
+    clientSocket.send(str.encode("Welcome to the server"))
+    while True:
+        data = clientSocket.recv(1024)
+        if not data:
+            break
+        message = data.decode()
+        print(message)
+    clientSocket.close()
+
+
+class Server:
+
+    def __init__(self, host, port, isOnline):
+        self.host = host
+        self.port = port
+        self.isOnline = isOnline
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def initializeServer(self):
+        self.socket.bind((self.host, self.port))
+
+    def waitForClients(self):
+        try:
+            # Maximum 10 clients can connect to the server
+            self.socket.listen(10)
+            print('Server is waiting for clients to connect')
+            while self.isOnline:
+                clientSocket, clientAddress = self.socket.accept()
+                print('Connected to: ' + clientAddress[0] + ':' + str(clientAddress[1]))
+                clientThread = threading.Thread(target=handleClientConnection, args=(clientSocket, clientAddress))
+                clientThread.start()
+        except KeyboardInterrupt:
+            self.closeServer()
+
+    def closeServer(self):
+        print('Shutting down the server')
+        self.socket.close()
