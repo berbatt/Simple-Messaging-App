9c093c96331e3362ceb74017c4d6410015ae9cf6
berbatt <berkay.bat@gmail.com>
2022-01-01 16:20:16 +0300

    Reading command-line arguments and server connection is implemented in mainClient

---
+++ mainClient.py
@@ -0,0 +1,19 @@
+from Client import Client
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        print("Arguments should be nickname, server's IP address and port number")
+        exit()
+
+    nickName = sys.argv[1]
+    hostAddress = sys.argv[2]
+    port = int(sys.argv[3])
+    client = Client(nickName=nickName, host=hostAddress, port=port)
+    client.initializeClient()
+    message = 'Hello world'
+    client.handleServerConnection(message=message)
+
+
+if __name__ == "__main__":
+    main()
